95372409ed2036c80c41b8c985b6e64786e14d3b
hnovak94 <hnovak94@charlottesvilleschools.org>
2022-01-27 15:21:53 +0000

    servo

--- code/servoTest.py
+++ code/servoTest.py
@@ -3,9 +3,10 @@ from time import sleep
 
 myGPIO = 18
 
-myCorrection = 0.45
-maxPW = (2.0 + myCorrection)/1000
-minPW = (1.0 - myCorrection)/1000
+# fixes servo range to be full 180 degrees
+correction = 0.45
+maxPW = (2.0 + correction)/1000
+minPW = (1.0 - correction)/1000
 
 servo = Servo(myGPIO, min_pulse_width = minPW, max_pulse_width = maxPW)
 
