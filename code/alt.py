7beaa7150a3702a444d6d4b48613b0b131716cb4
hnovak94 <hnovak94@charlottesvilleschools.org>
2022-01-27 14:48:28 +0000

    u

--- code/alt.py
+++ code/alt.py
@@ -1,6 +1,6 @@
 import time
 import board
-import adafruit_mpl3115a2
+import adafruit_mpl3115a2 # barometer library
 
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
@@ -10,17 +10,18 @@ sensor = adafruit_mpl3115a2.MPL3115A2(i2c)
 # Set this to a value in pascals:
 sensor.sealevel_pressure = 99077
 
-alt= []
-lv = sensor.altitude # initial value; zeroes it
+alt = [] # array for altitude values
+lv = sensor.altitude # initial value; zeroes it; 'launch value'
 
 while True:
 	altitude = lv - sensor.altitude 
 	# altitude is the diff between initial alt and current alt
 	alt.append(altitude)
 	# add latest data to alt array
-	print(alt) # print array
 
+	print(alt) # print array
 	print(alt[0]) # print first altitude
+
 	if len(alt) > 5: # if length of array is greater than 5
 		if altitude < alt[-5]: # if current altitude is less than before
 			print("apex") # will later be servos
