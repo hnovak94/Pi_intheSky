7aa5efca8ab4c9e0e978c883dbfa73e5551a6798
hnovak94 <hnovak94@charlottesvilleschools.org>
2022-01-27 15:14:04 +0000

    solved servo code

--- code/servoTest.py
+++ code/servoTest.py
@@ -1,12 +1,16 @@
 from gpiozero import Servo
 from time import sleep
 
-servo = Servo(18)
+myGPIO = 18
+
+myCorrection = 0.45
+maxPW = (2.0 + myCorrection)/1000
+minPW = (1.0 - myCorrection)/1000
+
+servo = Servo(myGPIO, min_pulse_width = minPW, max_pulse_width = maxPW)
 
 while True:
 	servo.min()
-	sleep(1)
-	servo.mid()
 	sleep(1)
 	servo.max()
 	sleep(1)
