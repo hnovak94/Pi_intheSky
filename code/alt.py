af3a5c572892a531b810a9171f54a340c9e9c8c7
hnovak94 <hnovak94@charlottesvilleschools.org>
2022-02-01 15:34:25 +0000

    working alt code

--- code/alt.py
+++ code/alt.py
@@ -8,13 +8,14 @@ i2c = board.I2C()  # uses board.SCL and board.SDA
 sensor = adafruit_mpl3115a2.MPL3115A2(i2c)
 
 # Set this to a value in pascals:
-sensor.sealevel_pressure = 99077
+sensor.sealevel_pressure = 102574
+time.sleep(0.5)
 
 alt = [] # array for altitude values
 lv = sensor.altitude # initial value; zeroes it; 'launch value'
 
 while True:
-	altitude = lv - sensor.altitude 
+	altitude = sensor.altitude - lv 
 	# altitude is the diff between initial alt and current alt
 	alt.append(altitude)
 	# add latest data to alt array
@@ -23,9 +24,9 @@ while True:
 	print(alt[0]) # print first altitude
 
 	if len(alt) > 5: # if length of array is greater than 5
-		if altitude < alt[-5]: # if current altitude is less than before
+		if max(alt) - altitude > 1: # if current altitude is less than before
 			print("apex") # will later be servos
-			time.sleep(1.0)
+			time.sleep(0.5)
 	
 
 
--- code/servoAlt.py
+++ code/servoAlt.py
@@ -8,17 +8,25 @@ servo = Servo(18)
 i2c = board.I2C()
 
 sensor = adafruit_mpl3115a2.MPL3115A2(i2c)
-num = 90
-sensor.sealevel_pressure = 101000
 
-while True: 
+sensor.sealevel_pressure = 102574
+time.sleep(0.5)
 
-	altitude = sensor.altitude
-	print(altitude)
-	if altitude > num:
-		servo.max()
-		time.sleep(1)
-		servo.mid()
-		time.sleep(1)
-		servo.min()
-		time.sleep(1)
+alt = [] # array for altitude values
+lv = sensor.altitude # initial value; zeroes it; 'launch value'
+
+while True:
+	altitude = sensor.altitude - lv 
+        # altitude is the diff between initial alt and current alt
+	alt.append(altitude)
+        # add latest data to alt array
+
+	print(alt) # print array
+	print(alt[0]) # print first altitude
+
+	if len(alt) > 5: # if length of array is greater than 5
+		if max(alt) - altitude > 1: # if current altitude is less than $
+			print("apex") # will later be servos
+
+			servo.max()
+			time.sleep(0.5)
