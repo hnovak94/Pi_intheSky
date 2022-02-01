95f35d92c63095d720b78ad872a1375c840ac2f4
hnovak94 <56082415+hnovak94@users.noreply.github.com>
2022-02-01 10:36:13 -0500

    added servo fix to code

--- code/servoAlt.py
+++ code/servoAlt.py
@@ -3,10 +3,17 @@ import board
 import adafruit_mpl3115a2
 from gpiozero import Servo
 
-servo = Servo(18)
+myGPIO = 18 # servo on pin 18
+
+# fixes servo range to be full 180 degrees
+# tutorial: https://www.raspberrypi-spy.co.uk/2018/02/basic-servo-use-with-the-raspberry-pi/
+correction = 0.45
+maxPW = (2.0 + correction)/1000
+minPW = (1.0 - correction)/1000
+
+servo = Servo(myGPIO, min_pulse_width = minPW, max_pulse_width = maxPW)
 
 i2c = board.I2C()
-
 sensor = adafruit_mpl3115a2.MPL3115A2(i2c)
 
 sensor.sealevel_pressure = 102574
