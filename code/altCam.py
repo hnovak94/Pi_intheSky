0542d84398cf05974f28def74ba135c601ac61d8
hnovak94 <56082415+hnovak94@users.noreply.github.com>
2022-02-03 09:17:30 -0500

    Update altCam.py

--- code/altCam.py
+++ code/altCam.py
@@ -15,22 +15,24 @@ alt = [] # array for altitude values
 lv = sensor.altitude # initial value; zeroes it; 'launch value'
 
 while True:
-	altitude = lv - sensor.altitude 
-	# altitude is the diff between initial alt and current alt
+	altitude = sensor.altitude - lv
+        # altitude is the diff between initial alt and current alt
 	alt.append(altitude)
-	# add latest data to alt array
+        # add latest data to alt array
 
 	print(alt) # print array
 	print(alt[0]) # print first altitude
 
 	if len(alt) > 5: # if length of array is greater than 5
-		if altitude < alt[-5]: # if current altitude is less than before
+		if max(alt) - altitude > 1: 
+		# if diff between max alt and current alt is greater than 1
+			print("apex")
 			with picamera.PiCamera() as camera:
-        camera.resolution = (1024, 768) # res
-        camera.start_preview()
-        sleep(1)
-        print("running")
-        camera.capture('../media/altcam.png')
-        print("done")
-		sleep(1.0)
+        			camera.resolution = (1024, 768) # res
+        			camera.start_preview()
+        			sleep(1)
+        			print("running")
+        			camera.capture('../media/altcam.png')
+        			print("done")
+				sleep(1.0)
 	
